7a921fff4b3905141d09c8e1b7e105d610e516a3
Tutaev Gleb <aka-laka-006@mail.ru>
2022-11-24 23:52:26 +0500

    JavaBegan

--- app/src/main/java/com/example/projectk/MainActivity.java
+++ app/src/main/java/com/example/projectk/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.projectk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonSighIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonSighIn = findViewById(R.id.buttonSighIn);
     }
 }
